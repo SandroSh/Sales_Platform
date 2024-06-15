8d92a7d13134b23fb657a3b2ff9d96fdc803fe4f
kiu_shubitidze.sandro <shubitidze.sandro@kiu.edu.ge>
2024-06-15 18:37:57 +0400

    Adding  navbar cart

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import GeorgianFlag from '../../assets/FlagOfGeorgia.svg'
 import BritainFlag from '../../assets/FlagOfGreatBritian.svg'
 import './navbar.css';
 import { useState } from 'react';
-
+import CartIcon from '../../assets/CartIcon.svg'
 const Navbar = () => {
   const [langBool, setLangBool] = useState<boolean>(true);
   return (
@@ -24,13 +24,20 @@ const Navbar = () => {
               <li>Contact</li>
             </Link>
           </ul>
+
           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
             <img src={ProfileImage} alt="Profile Image" style={{ width: '50px', aspectRatio: '1', borderRadius: '50%' }} />
             <div style={{ marginLeft: '10px', color: 'white' }}>Rebeca James</div>
+
           </div>
+
           <div className='language-signOut-cont'>
+            <div className='cart-container'>
+              <img src={CartIcon} alt="Cart Icon" />
+              <div><p>5</p></div>
+            </div>
             <img src={langBool ? GeorgianFlag : BritainFlag} alt="Georgian/Britain Flag" onClick={() => setLangBool(!langBool)} style={{ width: '40px', aspectRatio: '1', cursor: 'pointer' }} />
-            <Link to={'/signup'}>
+            <Link to={'/signin'}>
               <img src={SignOutIcon} alt="Sign Out Icon" style={{ width: '30px', marginLeft: '20px' }} />
             </Link>
           </div>
